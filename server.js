1900c39f1f3d9961e8ae71102806747aba7dbd20
Van4xx <van200529ter@gmail.com>
2025-01-07 15:34:46 +0300

    fsdfsdsfwsgsfsfdsпвпfsd

--- server.js
+++ server.js
@@ -4,13 +4,22 @@ const { Server } = require('socket.io');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["https://ruletka.top", "http://localhost:3000"],
+  methods: ["GET", "POST"],
+  credentials: true
+}));
+
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"]
-  }
+    origin: ["https://ruletka.top", "http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true
+  },
+  transports: ['websocket', 'polling'],
+  pingTimeout: 60000,
+  pingInterval: 25000
 });
 
 const waitingUsers = new Set();
