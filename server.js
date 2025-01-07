88fdde9c646a5d10047c2dbada8b5052a7262567
Van4xx <van200529ter@gmail.com>
2025-01-07 15:31:56 +0300

    fsdfsdsfwsgsfsfdsпвп

--- server.js
+++ server.js
@@ -42,66 +42,62 @@ io.on('connection', (socket) => {
       io.to(partner).emit('partnerLeft');
     }
 
-    // Удаляем из списка ожидания, если был там
-    waitingUsers.delete(socket.id);
-
-    // Ищем партнера среди ожидающих
-    let foundPartner = null;
-    for (const waitingUser of waitingUsers) {
-      const waitingSocket = io.sockets.sockets.get(waitingUser);
-      if (waitingSocket && waitingUser !== socket.id) {
-        foundPartner = waitingUser;
-        break;
-      }
+    // Проверяем, что пользователь не в списке ожидания
+    if (waitingUsers.has(socket.id)) {
+      log('User already in waiting list');
+      return;
     }
 
-    if (foundPartner) {
-      log('Found partner, connecting:', socket.id, 'with', foundPartner);
-      waitingUsers.delete(foundPartner);
+    // Ищем партнера
+    if (waitingUsers.size > 0) {
+      log('Found waiting users:', waitingUsers.size);
+      let partnerSocket = null;
       
-      const roomId = `room_${Date.now()}_${Math.random().toString(36).substr(2, 9)}`;
-      socket.join(roomId);
-      io.sockets.sockets.get(foundPartner).join(roomId);
-      
-      connectedPairs.set(socket.id, { partner: foundPartner, room: roomId });
-      connectedPairs.set(foundPartner, { partner: socket.id, room: roomId });
-      
-      // Отправляем событие начала чата обоим участникам
-      socket.emit('chatStarted', { roomId, isInitiator: true });
-      io.to(foundPartner).emit('chatStarted', { roomId, isInitiator: false });
-      
-      log('Users connected in room:', roomId);
+      for (const waitingUser of waitingUsers) {
+        if (waitingUser !== socket.id && io.sockets.sockets.get(waitingUser)) {
+          partnerSocket = waitingUser;
+          break;
+        }
+      }
+
+      if (partnerSocket) {
+        log('Connecting users:', socket.id, 'and', partnerSocket);
+        waitingUsers.delete(partnerSocket);
+        
+        const roomId = `${socket.id}-${partnerSocket}`;
+        socket.join(roomId);
+        io.sockets.sockets.get(partnerSocket).join(roomId);
+        
+        connectedPairs.set(socket.id, { partner: partnerSocket, room: roomId });
+        connectedPairs.set(partnerSocket, { partner: socket.id, room: roomId });
+        
+        io.to(roomId).emit('chatStarted', { roomId });
+      } else {
+        log('No available partners, adding to waiting list:', socket.id);
+        waitingUsers.add(socket.id);
+      }
     } else {
-      log('No partners available, adding to waiting list:', socket.id);
+      log('No waiting users, adding to waiting list:', socket.id);
       waitingUsers.add(socket.id);
-      socket.emit('waiting');
     }
   });
 
-  // Обработка WebRTC сигналов
-  socket.on('signal', ({ signal, roomId }) => {
-    log('Signal received from', socket.id, 'for room', roomId);
+  socket.on('sendSignal', ({ signal, to }) => {
+    console.log('Signal sent from', socket.id, 'to', to);
+    // Находим партнера через пару
     const pair = connectedPairs.get(socket.id);
-    if (pair && pair.room === roomId) {
-      log('Forwarding signal to partner:', pair.partner);
-      io.to(pair.partner).emit('signal', { signal, from: socket.id });
-    } else {
-      log('Invalid signal: no matching pair found');
+    if (pair) {
+      io.to(pair.partner).emit('receiveSignal', { signal, from: socket.id });
     }
+  });
+
+  socket.on('returnSignal', ({ signal, to }) => {
+    console.log('Return signal from', socket.id, 'to', to);
+    io.to(to).emit('receiveReturnSignal', { signal, from: socket.id });
   });
 
   socket.on('message', ({ roomId, message }) => {
-    log('Message received from', socket.id, 'for room', roomId);
-    const pair = connectedPairs.get(socket.id);
-    if (pair && pair.room === roomId) {
-      log('Forwarding message to partner:', pair.partner);
-      socket.to(roomId).emit('message', { 
-        message,
-        from: socket.id 
-      });
-    } else {
-      log('Invalid message: no matching pair found');
-    }
+    socket.to(roomId).emit('message', message);
   });
 
   socket.on('nextPartner', () => {
@@ -109,30 +105,24 @@ io.on('connection', (socket) => {
     if (currentPair) {
       const { partner, room } = currentPair;
       
-      // Отключаем текущую пару
       socket.leave(room);
       io.sockets.sockets.get(partner)?.leave(room);
+      
       connectedPairs.delete(socket.id);
       connectedPairs.delete(partner);
+      
       io.to(partner).emit('partnerLeft');
-      
-      // Запускаем новый поиск для обоих пользователей
-      socket.emit('searchingNewPartner');
-      io.to(partner).emit('searchingNewPartner');
-      
-      // Добавляем обоих в список ожидания
-      process.nextTick(() => {
-        if (io.sockets.sockets.get(partner)) {
-          waitingUsers.add(partner);
-          io.to(partner).emit('waiting');
-        }
-        socket.emit('startSearch');
-      });
     }
+    
+    // Автоматически начинаем новый поиск
+    socket.emit('searchingNewPartner');
+    process.nextTick(() => {
+      socket.emit('startSearch');
+    });
   });
 
   socket.on('disconnect', () => {
-    log('User disconnected:', socket.id);
+    console.log('User disconnected:', socket.id);
     onlineUsers--;
     io.emit('updateOnlineCount', onlineUsers);
     
@@ -146,12 +136,6 @@ io.on('connection', (socket) => {
       io.to(partner).emit('partnerLeft');
       connectedPairs.delete(socket.id);
       connectedPairs.delete(partner);
-      
-      // Добавляем партнера обратно в список ожидания
-      if (io.sockets.sockets.get(partner)) {
-        waitingUsers.add(partner);
-        io.to(partner).emit('waiting');
-      }
     }
   });
 });
@@ -165,7 +149,7 @@ setInterval(() => {
   }
 }, 10000);
 
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 5001;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 }); 
